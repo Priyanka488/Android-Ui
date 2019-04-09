7347213136463dbc92f56440bad97b2d2e88f187
Priyanka <500060668@stu.upes.ac.in>
2019-04-09 23:40:35 +0530

    Horizontal tabs

---
+++ Horizontal tab with header/app/src/main/java/com/example/android/dummy/LayoutDisplay.java
@@ -0,0 +1,60 @@
+package com.example.android.dummy;
+
+import android.content.res.Resources;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LayoutDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        // Create an instance of the tab layout from the view.
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setTabTextColors(getResources().
+                getColor(R.color.colorBlack), getResources().getColor(R.color.colorPrimary));
+        tabLayout.setBackgroundColor(getResources().
+                getColor(R.color.colorWhite));
+
+        // Set the text for each tab.
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+// Set the tabs to fill the entire layout.
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+// Use PagerAdapter to manage page views in fragments.
+
+        // Use PagerAdapter to manage page views in fragments.
+// Each page is represented by its own fragment.
+        final ViewPager viewPager = findViewById(R.id.pager);
+
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+// Setting a listener for clicks
+// Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new
+   TabLayout.OnTabSelectedListener() {
+       @Override
+       public void onTabSelected(TabLayout.Tab tab) {
+           viewPager.setCurrentItem(tab.getPosition());
+
+       }
+
+       @Override
+       public void onTabUnselected(TabLayout.Tab tab) {
+       }
+
+       @Override
+       public void onTabReselected(TabLayout.Tab tab) {
+       }
+   });
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Horizontal tab with header -
+
+<img src="ss/a.jpeg"></img>
+<img src="ss/b.jpeg"></img>
